82ce6e32025bc5b85b80f3d138df2feef4742598
Fidel Maulto <fidel.maluto77@gmail.com>
2026-07-16 16:26:54 +0100

    fix: Resolvendo o problema de conexão

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
+const dotenv = require('dotenv');
 const mysql = require('mysql2');
 
+dotenv.config();
+
 const db = mysql.createConnection({
-    host: process.env.db_host,
-    user: process.env.db_user,
-    password: process.env.db_password,
-    database: process.env.db_name,
-    port: process.env.db_port
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
