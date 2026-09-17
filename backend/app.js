89a97794fd963d76ffe862ffcb21505496d17174
Fidel Maulto <fidel.maluto77@gmail.com>
2026-09-17 01:40:56 +0100

    feat: Adicionando e criando as funções principais do projeto!

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
 // Chamando e executando o arquivo server.js
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+// Importação das Rotas
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Rotas
+app.use('/');
+app.use('/');
+app.use('/');
+app.use('/');
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
 // Responsável por chamar as rotas e a porta a escutar...
+const path = require('path');
+
+require("dotenv").config({
+    path: path.resolve(__dirname, "../.env")
+});
+
+import app from './app.js';
+
+const PORT = process.env.PORT || 3000;
+
+console.log('DB_USER: ', process.env.DB_USER);
+console.log('DB_PASSWORD: ', process.env.DB_PASSWORD);
+
+app.listem(PORT, () => {
+    console.log(`App rodando em: http://localhost:${PORT}`);
+});
