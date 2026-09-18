9eb52af2ebacd060e5676bea8b4a06d2b0b3f2b6
Fidel Maulto <fidel.maluto77@gmail.com>
2026-09-18 04:13:46 +0100

    fix: Ajustando o controller e a sitâxe da tabela utilizadores!

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,7 +17,7 @@ export const QueryUsers = async (req, res) => {
 export const CreateUser = async (req, res) => {
     const { nome, email, senhaCifrada, cargo } = req.body;
 
-    db.query('INSERT INOT utilizadores(nome, email, senhaCifrada, cargo) VALUES(?,?,?,?)',
+    db.query('INSERT INTO utilizadores(nome, email, senhaCifrada, cargo) VALUES(?,?,?,?)',
         [nome, email, senhaCifrada, cargo], (err, data) => {
             if (err) {
                 console.log('Erro ao cadastrar utilzador: ', err);
@@ -33,7 +33,7 @@ export const CreateUser = async (req, res) => {
 
 export const EditUser = async (req, res) => {
     const { id } = req.params;
-    const { nome, email, senhaCifrada, cargo } = req.bady;
+    const { nome, email, senhaCifrada, cargo } = req.body;
 
     db.query(`UPDATE utilizadores SET nome = ?, email = ?, senhaCifrada = ?, cargo = ? WHERE id = ?`,
         [nome, email, senhaCifrada, cargo, id], (err, data) => {
@@ -52,7 +52,7 @@ export const EditUser = async (req, res) => {
 export const DeleteUser = async (req, res) => {
     const { id } = req.params;
 
-    db.query('DELET FROM utilizadores WHERE id = ?', [id], (err, data) => {
+    db.query('DELETE FROM utilizadores WHERE id = ?', [id], (err, data) => {
         if (err) {
             console.log('Erro ao deletar utilizador: ', err);
             return res.status(500).json({ message: 'Erro ao deletar utilizador: ', err });
