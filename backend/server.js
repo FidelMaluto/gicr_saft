e6aae0552c4d31c1a25e46ebe7ca61c3c61e33a7
Fidel Maulto <fidel.maluto77@gmail.com>
2026-09-18 04:30:24 +0100

    refactor: Editando o server com profissionalismo!

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
-console.log('DB_USER: ', process.env.DB_USER);
-console.log('DB_PASSWORD: ', process.env.DB_PASSWORD);
-
 app.listen(PORT, () => {
     console.log(`App rodando em: http://localhost:${PORT}`);
 });
