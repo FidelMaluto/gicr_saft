3f7a89dcc10a865f2b3c38eb2cbfb0b01c865b4d
Fidel Maulto <fidel.maluto77@gmail.com>
2026-09-21 23:47:34 +0100

    fix: Corrijindo o problema do frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
+
+platformBrowser().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true,
+})
   .catch((err) => console.error(err));
+  
